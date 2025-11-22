662b515042602d5bb4baeae178bc654ee0c09a07
rome21081 <142682079+rome21081@users.noreply.github.com>
2025-11-22 20:38:54 +0800

    Add files via upload

---
+++ bootstrap/app.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Application;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',  // add this
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (\Illuminate\Foundation\Configuration\Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (\Illuminate\Foundation\Configuration\Exceptions $exceptions): void {
+        //
+    })
+    ->create();
